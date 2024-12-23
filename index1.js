59ffb2f083a833097e19cee620702af7fc03e578
VARUN JAIN <141307560+varun-jain03@users.noreply.github.com>
2024-12-24 00:00:22 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,12 @@
+const sum = require("./sum.js");
+const pro = require("./pro.js");
+
+let sumA = 3;
+let sumB = 5;
+let sumResult = sum(sumA, sumB);
+console.log(sumResult);
+
+let mulA = 4;
+let mulB = 6;
+let mulResult = pro(mulA, mulB);
+console.log(mulResult);
---
+++ pro.js
@@ -0,0 +1,5 @@
+function pro(a,b){
+    return a*b
+}
+
+module.export = pro;
